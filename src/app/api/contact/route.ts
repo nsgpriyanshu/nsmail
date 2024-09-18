b7bd4f9ca14ee55bfef92e864f20e1c39ae89450
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-09-18 18:58:19 +0530

    fix: api error

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,20 +3,29 @@ import { NextRequest, NextResponse } from 'next/server'
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL
 
 export async function POST(req: NextRequest) {
-  const { name, email, message } = await req.json()
-
-  if (!DISCORD_WEBHOOK_URL) {
-    return NextResponse.json(
-      { success: false, message: 'Discord Webhook URL is not defined' },
-      { status: 500 },
-    )
-  }
-
-  const discordMessage = {
-    content: `**New Contact Form Submission**\n\n**Name:** ${name}\n**Email:** ${email}\n**Message:**\n${message}`,
-  }
-
   try {
+    // Parse the JSON request body
+    const { name, email, message } = await req.json()
+
+    // Validate the request body
+    if (!name || !email || !message) {
+      return NextResponse.json({ success: false, message: 'Missing fields' }, { status: 400 })
+    }
+
+    // Check if the webhook URL is available
+    if (!DISCORD_WEBHOOK_URL) {
+      return NextResponse.json(
+        { success: false, message: 'Discord Webhook URL is not defined' },
+        { status: 500 },
+      )
+    }
+
+    // Prepare the message for Discord
+    const discordMessage = {
+      content: `**New Contact Form Submission**\n\n**Name:** ${name}\n**Email:** ${email}\n**Message:**\n${message}`,
+    }
+
+    // Send the message to Discord
     const discordResponse = await fetch(DISCORD_WEBHOOK_URL, {
       method: 'POST',
       headers: {
@@ -25,8 +34,9 @@ export async function POST(req: NextRequest) {
       body: JSON.stringify(discordMessage),
     })
 
+    // Check if the response was successful
     if (discordResponse.ok) {
-      return NextResponse.json({ success: true })
+      return NextResponse.json({ success: true, message: 'Message sent successfully' })
     } else {
       return NextResponse.json(
         { success: false, message: 'Failed to send message to Discord' },
@@ -34,6 +44,8 @@ export async function POST(req: NextRequest) {
       )
     }
   } catch (error) {
+    console.error('Error sending message to Discord:', error)
+
     return NextResponse.json(
       { success: false, message: 'Error sending message to Discord' },
       { status: 500 },
