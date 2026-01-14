d12237848ca6b473fba71fa0d021730276d26216
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2026-01-14 18:56:53 +0530

    chore: testing

---
+++ components/global/wrapper.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+interface Props {
+  className?: string
+  children: React.ReactNode
+}
+
+const Wrapper = ({ className, children }: Props) => {
+  return (
+    <section className={cn('mx-auto h-full w-full px-4 lg:max-w-7xl lg:px-20', className)}>
+      {children}
+    </section>
+  )
+}
+
+export default Wrapper
